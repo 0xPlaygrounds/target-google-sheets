b10110010138a71740097ef5901debb06d4e6685
0xMochan <mochanbot@gmail.com>
2022-07-07 14:24:36 -0500

    feat: improved models

--- target_google_sheets/models.py
+++ target_google_sheets/models.py
@@ -1,11 +1,17 @@
 from typing import Protocol, TypedDict
+from typing_extensions import NotRequired
 
+class _Sink(TypedDict):
+    default_sink_size: NotRequired[int]
+    sink_limit_increment: NotRequired[int]
+    max_sink_limit: NotRequired[int]
 
 class TargetGoogleSheetConfig(TypedDict):
     """Simple type definition for the target config"""
 
     spreadsheet_url: str
-
+    credentials_path: NotRequired[str]
+    sink: NotRequired[_Sink]
 
 class SingerData(Protocol):
     """Simplified representation of singer data"""
